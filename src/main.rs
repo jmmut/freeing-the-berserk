200d0c8ceabb61658d2450ac1702d084703053aa
jose miguel mut <jomutlo@gmail.com>
2026-07-23 18:57:13 +0200

    basic movement

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ async fn main() {
     let map_width_meters = 20.0;
     let mut pos = vec2(0.0, 0.0);
     let mut size = vec2(1.0, 1.0);
+    let mut speed = vec2(0.05, 0.05);
     loop {
         let screen = vec2(screen_width(), screen_height());
         let meters_to_pixels = screen.x / map_width_meters;
@@ -13,11 +14,26 @@ async fn main() {
         if is_key_pressed(KeyCode::Escape) {
             break;
         }
-        draw_rectangle((pos.x - size.x * 0.5) * meters_to_pixels + screen.x *0.5, (pos.y - size.y * 0.5) * meters_to_pixels + screen.y * 0.5, size.x * meters_to_pixels, size.y * meters_to_pixels, SKYBLUE);
+        if is_key_down(KeyCode::W) {
+            pos.y -= speed.y;
+        }
+        if is_key_down(KeyCode::S) {
+            pos.y += speed.y;
+        }
+        if is_key_down(KeyCode::A) {
+            pos.x -= speed.x;
+        }
+        if is_key_down(KeyCode::D) {
+            pos.x += speed.x;
+        }
+        draw_rectangle(
+            (pos.x - size.x * 0.5) * meters_to_pixels + screen.x * 0.5,
+            (pos.y - size.y * 0.5) * meters_to_pixels + screen.y * 0.5,
+            size.x * meters_to_pixels,
+            size.y * meters_to_pixels,
+            SKYBLUE,
+        );
 
         next_frame().await
     }
 }
-
-
-
